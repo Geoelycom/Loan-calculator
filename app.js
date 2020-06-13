1e98e9d02b723aee7ac2ac456c690b89d45befbd
elyan <ekenimohelyanpheta@gmail.com>
2020-06-13 23:14:00 +0100

    updated html pages

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-alert(1);
+//listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+
+// calculate Results
+function calculateResults(e) {
+
+    //console.log('calculating...')
+    // UI varaibles
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+
+    // compute Monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    } else {
+        console.log('please check your value');
+
+    }
+
+
+
+
+
+    e.preventDefault();
+}
